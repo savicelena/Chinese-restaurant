64613f226f9762a95ae39b9a0ddf3ae041fd9890
Elena <elenasavic5@gmail.com>
2024-05-27 16:36:31 +0200

    Osnova aplikacije

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <!-- <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> -->
+    <h1>Hello</h1>
   </nav>
   <router-view/>
 </template>
