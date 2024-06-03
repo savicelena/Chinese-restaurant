a1bcd126200d9b9326a6293c6994ae7305c35434
magdalena0711 <om210304d@student.etf.bg.ac.rs>
2024-06-03 22:06:48 +0200

    Slucajno zakomentarisala jelovnik

--- src/views/MenuFood.vue
+++ src/views/MenuFood.vue
@@ -1,209 +1,251 @@
 <template>
-  <div class="my-account grad">
-      
-      <hr>
-      <div class="tabs">
+    <div class="container-xxl py-5">
+      <div class="container">
+        <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+          <div class="download-menu">
+            <h1 class="mb-3">{{$t('Food menu')}}</h1>
+            <a href="/files/menu.pdf" download>
+              <img src="@/assets/icons/download.jpg" alt="Download Menu" width="20px" height="20px">
+              <div class="menu-text">
+                <span class="bold">{{$t('Download Menu')}}</span>
+              </div>
+            </a>
+          </div>
+          <!-- Dugme za sortiranje i polja za pretraživanje -->
+          <div class="sort-menu mt-3">
+            <button class="sort-button" @click="toggleDropdown">
+              {{$t('Sort')}}
+            </button>
+            <ul v-if="showDropdown" class="dropdown-menu">
+              <li>
+                <a class="dropdown-item" href="#" @click.prevent="sortMenu('price')">{{ $t('Sort by Price') }}</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#" @click.prevent="sortMenu('name')">{{ $t('Sort by Name') }}</a>
+              </li>
+            </ul>
+          </div>
+          <div class="search-menu mt-3">
+            <input type="text" v-model="searchQueryName" @input="filterMenu" :placeholder="$t('Search by Name')">
+          </div>
+          <div class="search-menu mt-3">
+            <input type="text" v-model="searchQueryPrice" @input="filterMenu" :placeholder="$t('Search by Price')">
+          </div>
+        </div>
+        <div class="menu">
           <nav class="navbar">
-              <a href="#" @click.prevent="changeTab(0)">
-                  <div class="tab-text">
-                      <span class="bold">{{ tr(msg.myCart) }}</span>
-                  </div>
-              </a>
-              <a href="#" @click.prevent="changeTab(1)">
-                  <div class="tab-text">
-                      <span class="bold">{{ tr(msg.myOrders) }}</span>
-                  </div>
-              </a>
+            <a href="#" @click.prevent="changeComponent(0)">
+              <img src="@/assets/icons/mainCourse.png" alt="Main Course">
+              <div class="menu-text">
+                <span class="bold">{{$t('mainCourse')}}</span>
+              </div>
+            </a>
+            <a href="#" @click.prevent="changeComponent(1)">
+              <img src="@/assets/icons/desserts.png" alt="Dessert">
+              <div class="menu-text">
+                <span class="bold">{{$t('dessert')}}</span>
+              </div>
+            </a>
+            <a href="#" @click.prevent="changeComponent(2)">
+              <img src="@/assets/icons/appetizer.png" alt="Appetizer">
+              <div class="menu-text">
+                <span class="bold">{{$t('appetizer')}}</span>
+              </div>
+            </a>
           </nav>
-      </div>
-      <div class="content">
-          <div v-if="selectedTab === 0">
-              <h3>{{ tr(msg.myCart) }}:</h3>
-              <table class="table">
-                  <thead v-show="myCart.length > 0">
-                      <tr>
-                          <th scope="col">#</th>
-                          <th scope="col">{{ tr(msg.item) }}</th>
-                          <th scope="col">{{ tr(msg.quantity) }}</th>
-                          <th scope="col">{{ tr(msg.remove) }}</th>
-                      </tr>
-                  </thead>
-                  <tbody>
-                      <tr v-for="(item, index) in myCart" :key="index">
-                          <th scope="row">{{ index + 1 }}</th>
-                          <td>{{ item.name }}</td>
-                          <td>{{ item.quantity }}</td>
-                          <td class="delbut">
-                              <button class="btn btn-outline-dark" @click="removeFromCart(item)">
-                                  {{ tr(msg.remove) }}
-                              </button>
-                          </td>
-                      </tr>
-                  </tbody>
-              </table>
+        </div>
+        <div class="col-lg-10 col-md-9 col-sm-12" id="main">
+          <div v-if="chosen[0]">
+            <main-course :items="filteredItems" @food-selected="navigateToFoodDetail"></main-course>
           </div>
-          <div v-if="selectedTab === 1">
-              <h3>{{ tr(msg.myOrders) }}:</h3>
-              <table class="table table-striped">
-                  <thead v-show="myOrders.length > 0">
-                      <tr>
-                          <th scope="col">#</th>
-                          <th scope="col">{{ tr(msg.orderId) }}</th>
-                          <th scope="col">{{ tr(msg.date) }}</th>
-                          <th scope="col">{{ tr(msg.total) }}</th>
-                          <th scope="col">{{ tr(msg.status) }}</th>
-                      </tr>
-                  </thead>
-                  <tbody>
-                      <tr v-for="(order, index) in myOrders" :key="index">
-                          <th scope="row">{{ index + 1 }}</th>
-                          <td>{{ order.id }}</td>
-                          <td>{{ order.date }}</td>
-                          <td>{{ order.total }}</td>
-                          <td>{{ order.status }}</td>
-                      </tr>
-                  </tbody>
-              </table>
+          <div v-if="chosen[1]">
+            <dessert :items="filteredItems" @food-selected="navigateToFoodDetail"></dessert>
           </div>
+          <div v-if="chosen[2]">
+            <appetizer :items="filteredItems" @food-selected="navigateToFoodDetail"></appetizer>
+          </div>
+        </div>
       </div>
-  </div>
-</template>
-
-<style scoped>
-.grad {
-  background-color: rgb(229, 158, 158);
-}
-
-.my-account,
-th,
-td,
-tr,
-a {
-  color: rgb(0, 0, 0);
-  font-size: 18px;
-}
-
-h6 {
-  font-weight: bold;
-}
-
-.welcome {
-  display: flex;
-  justify-content: space-between;
-}
-
-.sobtn {
-  margin: 10px;
-}
-
-.tabs {
-  display: flex;
-  justify-content: center;
-  margin-bottom: 20px;
-}
-
-.navbar {
-  display: flex;
-  gap: 20px;
-}
-
-.navbar a {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-decoration: none;
-  color: inherit;
-}
-
-.tab-text {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.tab-text .bold {
-  font-weight: bold;
-  color: #000;
-}
-
-.content {
-  margin-top: 20px;
-}
-</style>
-
-<script>
-import { mapGetters, mapActions } from 'vuex';
-
-export default {
-  name: 'MyAccount',
-  data() {
+    </div>
+  </template>
+  
+  <script>
+  import MainCourse from "@/components/MainCourse.vue";
+  import Dessert from "@/components/DessertMenu.vue";
+  import Appetizer from "@/components/AppetizerMenu.vue";
+  import mainCourses from "@/data/mainCourse.json";
+  import desserts from "@/data/desserts.json";
+  import appetizers from "@/data/appetizers.json";
+  
+  export default {
+    name: "MenuFood",
+    components: {
+      MainCourse,
+      Dessert,
+      Appetizer
+    },
+    data() {
       return {
-          selectedTab: 0,
-          msg: {
-              welcome: {
-                  sr: 'Dobrodošli',
-                  en: 'Welcome'
-              },
-              myCart: {
-                  sr: 'Moja korpa',
-                  en: 'My Cart'
-              },
-              item: {
-                  sr: 'Proizvod',
-                  en: 'Item'
-              },
-              quantity: {
-                  sr: 'Količina',
-                  en: 'Quantity'
-              },
-              remove: {
-                  sr: 'Ukloni',
-                  en: 'Remove'
-              },
-              signOut: {
-                  sr: 'Odjavi se',
-                  en: 'Sign Out'
-              },
-              myOrders: {
-                  sr: 'Moje narudžbine',
-                  en: 'My Orders'
-              },
-              orderId: {
-                  sr: 'ID narudžbine',
-                  en: 'Order ID'
-              },
-              date: {
-                  sr: 'Datum',
-                  en: 'Date'
-              },
-              total: {
-                  sr: 'Ukupno',
-                  en: 'Total'
-              },
-              status: {
-                  sr: 'Status',
-                  en: 'Status'
-              }
+        chosen: [true, false, false],
+        sorted: false,
+        sortType: '',
+        showDropdown: false,
+        searchQueryName: '',
+        searchQueryPrice: '',
+        foodData: mainCourses // Početni podaci su za glavna jela
+      };
+    },
+    computed: {
+      sortedItems() {
+        let sortedData = [...this.foodData];
+        if (this.sorted) {
+          if (this.sortType === 'price') {
+            sortedData.sort((a, b) => parseFloat(a.price.replace('$', '')) - parseFloat(b.price.replace('$', '')));
+          } else if (this.sortType === 'name') {
+            sortedData.sort((a, b) => a.name.localeCompare(b.name));
           }
+        }
+        return sortedData;
+      },
+      filteredItems() {
+        let filteredData = this.sortedItems;
+        if (this.searchQueryName) {
+          filteredData = filteredData.filter(item => item.name.toLowerCase().includes(this.searchQueryName.toLowerCase()));
+        }
+        if (this.searchQueryPrice) {
+          filteredData = filteredData.filter(item => item.price.replace('$', '').includes(this.searchQueryPrice));
+        }
+        return filteredData;
       }
-  },
-  computed: {
-      myCart() {
-          return this.$store.state.cartItems;
+    },
+    methods: {
+      changeComponent(num) {
+        this.chosen = [false, false, false];
+        this.chosen[num] = true;
+        if (num === 0) {
+          this.foodData = mainCourses;
+        } else if (num === 1) {
+          this.foodData = desserts;
+        } else if (num === 2) {
+          this.foodData = appetizers;
+        }
       },
-      myOrders() {
-          return this.$store.state.orders.filter(order => order.accountId === this.account.id);
+      sortMenu(type) {
+        this.sortType = type;
+        this.sorted = true;
+        this.showDropdown = false;
       },
-      ...mapGetters(['account'])
-  },
-  methods: {
-      ...mapActions(['signOut', 'removeFromCart']),
-      changeTab(num) {
-          this.selectedTab = num;
+      toggleDropdown() {
+        this.showDropdown = !this.showDropdown;
       },
-      tr(messages) {
-          return messages[this.$store.state.lang];
-      }
+      filterMenu() {
+        this.searchQueryName = this.searchQueryName.trim();
+        this.searchQueryPrice = this.searchQueryPrice.trim();
+      },
+    viewDetail(item) {
+      this.$router.push({ name: 'FoodDetail', params: { food: item } });
+    }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .menu {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 20px;
   }
-}
-</script>
+  
+  .navbar {
+    display: flex;
+    gap: 20px;
+  }
+  
+  .navbar a {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-decoration: none;
+    color: inherit;
+  }
+  
+  .navbar a img {
+    width: 40px;
+    height: 40px;
+  }
+  
+  .menu-text {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  
+  .menu-text .bold {
+    font-weight: bold;
+    color: #000;
+  }
+  
+  .sort-menu {
+    position: relative;
+    display: inline-block;
+  }
+  
+  .sort-button {
+    background-color: #ff9900; 
+    color: white;
+    border: none;
+    padding: 10px 20px;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 16px;
+  }
+  
+  .sort-button:hover {
+    background-color: #e68a00; 
+  }
+  
+  .dropdown-menu {
+    margin-top: 10px;
+    border-radius: 5px;
+    border: 1px solid #ddd;
+    background-color: white;
+    position: absolute;
+    left: 0;
+    top: 100%;
+    z-index: 1000;
+    display: block;
+    width: 100%;
+    padding: 0;
+  }
+  
+  .dropdown-item {
+    cursor: pointer;
+    padding: 10px;
+    font-size: 16px;
+    display: block;
+    width: 100%;
+    clear: both;
+    color: #000;
+    text-align: inherit;
+    white-space: nowrap;
+    background-color: transparent;
+    border: 0;
+  }
+  
+  .dropdown-item:hover {
+    background-color: #f0f0f0;
+  }
+  
+  .search-menu {
+    display: inline-block;
+    margin-left: 10px;
+  }
+  
+  .search-menu input {
+    padding: 10px;
+    font-size: 16px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+  </style>
+  
